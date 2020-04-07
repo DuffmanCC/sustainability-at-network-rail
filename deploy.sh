839012d4ac43c26b57bf48c398ac8481f1408350
Carlos Ortiz <carlosor@gmail.com>
2020-04-07 22:05:12 +0100

    update config

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:duffmancc/dev-sustainability-at-network-rail.git master:gh-pages
+git push -f git@github-NR:network-rail/sustainability-at-network-rail.git master:gh-pages
 
 cd -
